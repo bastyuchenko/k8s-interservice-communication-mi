383f3f3d0a6dfc146922ebdfedb26443cec30275
ABASTIU <Anton_Bastiuchenko@epam.com>
2022-09-11 23:01:17 +0300

    Initial commit 2 services

---
+++ Service1/Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Service1.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ILogger<ValuesController> _logger;
+
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new[] { "Service2.value1", "Service2.value2" };
+        }
+    }
+}
